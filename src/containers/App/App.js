2f6e2a6b6fce4c1201496780306f07a75aaceb55
justinwasserstein <justin.wasserstein@gmail.com>
2021-01-28 07:23:52 -0500

    Add <LandingPage>

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,20 +1,38 @@
+import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
+import LandingPage from '../LandingPage';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <Router>
-                <Navbar username='@jwasserstein' logOut={() => {}} />
-                <Switch>
-                    
-                </Switch>
-                <Footer />
-            </Router>
-        </div>
-    );
+class App extends Component {
+    componentDidMount(){
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    updateWindowDimensions(){
+        const html = document.querySelector('html');
+        html.style.minHeight = `${window.innerHeight}px`;
+    }
+    
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <Navbar username='@jwasserstein' logOut={() => {}} />
+                    <Switch>
+                        <Route path='/' component={LandingPage} />
+                    </Switch>
+                    <Footer />
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
